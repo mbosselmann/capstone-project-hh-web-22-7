9390db84212e7c7abe1404a1e6bf1dae51eb113e
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-22 14:20:15 +0100

    Test excalidraw

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,45 @@
-export default function HomePage() {
-  return <h1>Hello from Next.js</h1>;
+import { useState, useEffect } from "react";
+import initialData from "@/initialData.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+
+  & > div > .App-top-bar {
+    align-items: flex-start;
+  }
+
+  & > div > div > section .Stack {
+    display: flex;
+    flex-direction: column;
+  }
+
+  & > div > div > section > div > div > .mobile-misc-tools-container {
+    display: none;
+  }
+`;
+
+export default function IndexPage() {
+  const [Component, setComponent] = useState(null);
+  useEffect(() => {
+    import("@excalidraw/excalidraw").then((comp) => {
+      setComponent(comp.Excalidraw);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1> Excalidraw with Next </h1>
+      <div style={{ height: "500px", margin: "0.2rem" }}>
+        {Component && (
+          <Container>
+            <Component
+              UIOptions={{ welcomeScreen: false }}
+              initialData={initialData}
+            />
+          </Container>
+        )}
+      </div>
+    </>
+  );
 }
